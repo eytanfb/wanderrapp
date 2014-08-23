a9c8677a0e99b871750a9daa0bf7da947e98f34c
Eytan Anjel <anjel@usc.edu>
2014-08-23 14:10:51 -0700

    edited gitignore. small css changes

--- platforms/ios/www/js/controllers/myself_controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('wanderr.controllers').controller("MyselfCtrl", function($scope, UserService) {
-  console.log("myself");
-  $scope.user = UserService.user;
-  console.log($scope.user);
-  $scope.profilePicture = $scope.user.thirdPartyUserData.picture.data.url;
-});
